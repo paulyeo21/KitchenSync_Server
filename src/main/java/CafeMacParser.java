78df7e51c3d0a5eac963544507eb910d377a8173
Paul Yeo <kyeo@macalester.edu>
2014-04-11 19:17:39 -0500

    comment out database_url override option

--- src/main/java/CafeMacParser.java
+++ src/main/java/CafeMacParser.java
@@ -55,8 +55,8 @@ public class CafeMacParser {
 
     private static SessionFactory createSessionFactory() {
         Configuration configuration = new Configuration().configure();
-        if(System.getenv("DATABASE_URL") != null)
-            configuration.setProperty("hibernate.connection.url", System.getenv("DATABASE_URL"));
+//        if(System.getenv("DATABASE_URL") != null)
+//            configuration.setProperty("hibernate.connection.url", System.getenv("DATABASE_URL"));
         return configuration.buildSessionFactory(
                 new StandardServiceRegistryBuilder()
                         .applySettings(configuration.getProperties())
